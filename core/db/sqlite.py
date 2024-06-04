9e7021fdc7b9409a1ee94bad862ae944df15c27a
sanlopez <irene.sanchezl93@gmail.com>
2024-06-04 12:44:39 +0200

    ddbb location can be specified

--- core/db/sqlite.py
+++ core/db/sqlite.py
@@ -1,10 +1,14 @@
 import os
 from sqlite3 import dbapi2 as sqlite
 from core.constants import DBNAME
+import configparser
 
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'config.yaml'))
+ddbb_path = config['DDBB'].get('DDBB_PATH')
 
 def connect_to_ddbb():
-    connection = sqlite.connect(database=os.path.join(os.path.dirname(os.path.abspath(__file__)), DBNAME))
+    connection = sqlite.connect(database=os.path.join(ddbb_path, DBNAME))
     create_ddbb_data(connection)
     return connection
 
