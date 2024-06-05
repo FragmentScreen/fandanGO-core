71fe0eab2f6f6ec038eaed2eefbcec0e8dd4c7d4
sanlopez <irene.sanchezl93@gmail.com>
2024-06-05 18:25:54 +0200

    when performing 'execute' help you can see the actions availables at facility-techinique plugin

--- core/actions/other_plugin_action.py
+++ core/actions/other_plugin_action.py
@@ -26,16 +26,43 @@ def delegate_action_to_plugin():
     exit_with_errors = False
 
     if '--help' in unknown_args and (not parsed_args.action or not parsed_args.name):
-        final_parser = argparse.ArgumentParser(parents=[parser_used],
-                                               usage=f'{invoke_cmd} [--help] --action ACTION_NAME --name PROJECT_NAME',
-                                               epilog=f'Example: {invoke_cmd} --action copy-data --name test_project\n\n',
-                                               add_help=True)
-        final_parser.print_help()
-        final_parser.exit(0)
+        if not parsed_args.name:
+            final_parser = argparse.ArgumentParser(parents=[parser_used],
+                                                   usage=f'{invoke_cmd} [--help] --action ACTION_NAME --name PROJECT_NAME',
+                                                   epilog=f'Example: {invoke_cmd} --action copy-data --name test_project\n\n',
+                                                   add_help=True)
+            final_parser.print_help()
+            final_parser.exit(0)
+
+        elif parsed_args.name and not parsed_args.action:
+            if check_if_project_exists(parsed_args.name):
+                # get project plugin manager
+                plugin_manager = get_plugin_manager(parsed_args.name)
+
+                if plugin_manager:
+                    # import plugin_manager python module
+                    plugin_entry_point = entry_points().select(group='fandango.plugin', name=plugin_manager)
+                    plugin_module_name = next(iter(plugin_entry_point), None).value
+                    plugin_module = import_module(plugin_module_name)
+                    plugin_module.Plugin.define_methods()
+                    available_actions = '{' + ', '.join(plugin_module.Plugin.get_methods().keys()) + '}'
+
+                    final_parser = argparse.ArgumentParser(parents=[parser_used],
+                                                           usage=f'{invoke_cmd} [--help] --action {available_actions} --name PROJECT_NAME',
+                                                           add_help=True)
+                    final_parser.print_help()
+                    final_parser.exit(0)
+
+                else:
+                    print(f'The project {parsed_args.name} does not have a plugin manager associated. Execute "{FANDANGO_CMD} {ACTION_LINK_PROJECT} --help" for more details')
+                    exit_with_errors = True
+            else:
+                print(f'There is no project with name {parsed_args.name}. Check the existing projects with "{FANDANGO_CMD} {ACTION_LIST_PROJECTS}"')
+                exit_with_errors = True
 
     else:
         if not parsed_args.name or not parsed_args.action:
-            print('You should provided a project name and the action to be performed!')
+            print('You should provide a project name and the action to be performed!')
             exit_with_errors = True
 
         else:
